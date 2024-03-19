b3433159a7c52a2e571d3c5fb791711048543539
ohshitnotgood <98031796+ohshitnotgood@users.noreply.github.com>
2024-03-19 15:22:41 +0100

    Added demo for FadeInUp animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-import type { Component } from 'solid-js';
+import { createSignal, type Component } from 'solid-js';
+import FadeInUp from './components/FadeInUp';
 
 const App: Component = () => {
+    const [animationTrigger, setAnimationTrigger] = createSignal(false);
+
+    function triggerAnimation() {
+        setAnimationTrigger(true)
+    }
+
     return (
         <div class="font-bold">
-            Hello World
+            <button onclick={triggerAnimation}>
+                Trigger FadeInUp animation
+            </button>
+
+            <FadeInUp class='font-mono text-red-600' delay={0} trigger={animationTrigger}>
+                FADE IN UP
+            </FadeInUp>
         </div>
     );
 };
